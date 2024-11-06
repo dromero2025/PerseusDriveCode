1536991eb523ed951e46d98258dcb8e07375a3f3
Dee <dromero@bancroftschool.org>
2024-11-06 16:35:22 -0500

    autons, skills, edits

--- src/main.cpp
+++ src/main.cpp
@@ -19,23 +19,27 @@ brain Brain;
 controller Controller = controller();
 
 //drivetrain initializations
-motor fLMotor = motor(PORT18, ratio36_1, false);//front left drive motor
-motor fRMotor = motor(PORT13, ratio36_1, true);//front right drive motor
-motor rLMotor = motor(PORT19, ratio36_1, false);//rear left drive motor
-motor rRMotor = motor(PORT14, ratio36_1, true);//rear right drive motor
+motor fLMotor = motor(PORT18, ratio6_1, false);//front left drive motor
+motor fRMotor = motor(PORT13, ratio6_1, true);//front right drive motor
+motor rLMotor = motor(PORT19, ratio6_1, false);//rear left drive motor
+motor rRMotor = motor(PORT14, ratio6_1, true);//rear right drive motor
 
 inertial inert = inertial(PORT5);
 
 motor_group leftDrive = motor_group(fLMotor, rLMotor);
 motor_group rightDrive = motor_group(fRMotor, rRMotor);
+motor_group drive = motor_group(fLMotor, rLMotor, fRMotor, rRMotor);
+
+float driveChanger = 0.75;
 
 //smartdrive blackjack = smartdrive(leftDrive, rightDrive, inert, 12.57, 10.625, 9.5, inches, 2);
 
 //intake intializations
 motor hIntMotor = motor(PORT11, ratio18_1, true);
-motor lIntMotor = motor(PORT20, ratio18_1, true);
+motor lIntMotor = motor(PORT20, ratio6_1, true);
 
 motor_group intake = motor_group(hIntMotor, lIntMotor);
+motor_group drivetake = motor_group(fLMotor, rLMotor, fRMotor, rRMotor, hIntMotor, lIntMotor);
 
 //sensor intializations
 aivision windshield = aivision(PORT6);
@@ -44,13 +48,17 @@ aivision windshield = aivision(PORT6);
   aivision::colordesc RED2 = aivision::colordesc(2, 203, 32, 95, 10, 0.2);
   aivision::colordesc RED3 = aivision::colordesc(3, 219, 46, 106, 10, 0.2);
   aivision::colordesc RED4 = aivision::colordesc(4, 245, 121, 184, 10, 0.2);
+  color RedAvg = color((168 + 203 + 219 + 245)/4, (9 + 32 + 46 + 121)/4, (59 + 95 + 106 + 184)/4);
+
+  aivision::codedesc redRing = aivision::codedesc(1, RED1, RED2, RED3, RED4);
   //blue ring
 
   //green goal
   aivision::colordesc GREEN0 = aivision::colordesc(5, 124, 241, 93, 10, 0.2);
   aivision::colordesc GREEN1 = aivision::colordesc(6, 101, 199, 82, 10, 0.2);
-//distance back = distance(PORT3);
-aivision backupCam = aivision(PORT4);
+
+  aivision::codedesc goal = aivision::codedesc(3, GREEN0, GREEN1);
+distance back = distance(PORT6);
 
 //intake redirect intitializations
 motor liftLeft = motor(PORT2, ratio18_1, true);
@@ -105,11 +113,22 @@ void intakeDownTwo(){
 }
 
 //auton methods
-/* void redDetect(){
-  if(){
-
+void redDetect(){
+  windshield.takeSnapshot(redRing, 8);
+  if(windshield.objectCount > 0){
+    Brain.Screen.drawRectangle(windshield.objects[0].originX, windshield.objects[0].originY, windshield.objects[0].width, windshield.objects[0].height, RedAvg);
+  } else {
+    Brain.Screen.print("failed");
   }
-} */
+}
+void approachRed(){
+  redDetect();
+  while(windshield.objects[0].originX != 0){
+    leftDrive.spin(directionType::rev, 50, velocityUnits::pct);
+    leftDrive.spin(directionType::fwd, 50, velocityUnits::pct);
+  }
+
+}
 
 
 
@@ -124,10 +143,25 @@ void intakeDownTwo(){
 /*---------------------------------------------------------------------------*/
 
 void pre_auton(void) {
+  mogoClamp.set(true);
+  inert.calibrate();
+  while(inert.isCalibrating() == true){
+    Brain.Screen.clearLine();
+    Brain.Screen.print("inertial calibrating, please wait");
+  }
+  Brain.Screen.clearLine();
 
+  drive.resetPosition();
+  intake.resetPosition();
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
 }
+
+
+
+
+//PID code
+
 
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
@@ -140,8 +174,62 @@ void pre_auton(void) {
 /*---------------------------------------------------------------------------*/
 
 void autonomous(void) {
-  
+  //auton for mogo, preload onto, turn, intake, right side
+  /*
+  intake.setVelocity(100, velocityUnits::pct);
+  drive.spinFor(directionType::rev, 2000, rotationUnits::deg, 50, velocityUnits::pct);
+  drive.stop();
+  mogoClamp.set(false);
+  intake.spinFor(directionType::fwd, 1000, rotationUnits::deg, 100, velocityUnits::pct);
+  drive.resetPosition();
+  leftDrive.spinFor(directionType::rev, 550, rotationUnits::deg, 25, velocityUnits::pct);
+  rightDrive.spinFor(directionType::rev, -750, rotationUnits::deg, 25, velocityUnits::pct);
+  drive.resetPosition();
+  intake.spin(directionType::fwd);
+  drivetake.spinFor(directionType::fwd, 1750, rotationUnits::deg, 100, velocityUnits::pct);
+  intake.stop();
+  drive.spinFor(directionType::rev, 500, rotationUnits::deg, 100, velocityUnits::pct);
+  drive.stop();
+  drive.resetPosition();
+  rightDrive.spinFor(directionType::rev, 550, rotationUnits::deg, 25, velocityUnits::pct);
+  leftDrive.spinFor(directionType::rev, -350, rotationUnits::deg, 25, velocityUnits::pct);
+  drive.resetPosition();
+  intake.spin(directionType::fwd);
+  drive.spinFor(directionType::fwd, 2700, rotationUnits::deg, 100, velocityUnits::pct);
+  wait(1000, timeUnits::msec);
+  intake.stop();
+  */
+
+  //programmer skills
+  drive.spinFor(directionType::rev, 1200, rotationUnits::deg, 50, velocityUnits::pct);
+  mogoClamp.set(false);
+  intake.spinFor(directionType::fwd, 500, rotationUnits::deg, 100, velocityUnits::pct);
+  drive.resetPosition();
+  rightDrive.spinFor(directionType::rev, 550, rotationUnits::deg, 25, velocityUnits::pct);
+  leftDrive.spinFor(directionType::rev, -750, rotationUnits::deg, 25, velocityUnits::pct);
+  intake.setVelocity(100, velocityUnits::pct);
+  intake.spin(directionType::fwd);
+  drive.spinFor(directionType::fwd, 2000, rotationUnits::deg, 100, velocityUnits::pct);
+  wait(1000, timeUnits::msec);
+  drive.spinFor(directionType::fwd, 2000, rotationUnits::deg, 100, velocityUnits::pct);
+  wait(500, timeUnits::msec);
+  intake.stop();
+
+  drive.resetPosition();
+  drive.spinFor(directionType::rev, 1500, rotationUnits::deg, 100, velocityUnits::pct);
+  drive.resetPosition();
+  rightDrive.spinFor(directionType::rev, -750, rotationUnits::deg, 25, velocityUnits::pct);
+  leftDrive.spinFor(directionType::rev, 550, rotationUnits::deg, 25, velocityUnits::pct);
+  intake.setVelocity(100, velocityUnits::pct);
+  drive.spinFor(directionType::rev, 1500, rotationUnits::deg, 100, velocityUnits::pct);
+  wait(1000, timeUnits::msec);
+  intake.stop();
+
+
 }
+
+
+
 
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
@@ -158,8 +246,8 @@ void usercontrol(void) {
   while (1) {
 
   //drivetrain code
-    leftDrive.spin(directionType::fwd, Controller.Axis3.value() + Controller.Axis1.value(), velocityUnits::pct);
-    rightDrive.spin(directionType::fwd, Controller.Axis3.value() - Controller.Axis1.value(), velocityUnits::pct);
+    leftDrive.spin(directionType::fwd, (Controller.Axis3.value() + Controller.Axis1.value()) * driveChanger, velocityUnits::pct);
+    rightDrive.spin(directionType::fwd, (Controller.Axis3.value() - Controller.Axis1.value()) * driveChanger, velocityUnits::pct);
     
   //intake code 
     //intake press
@@ -168,7 +256,6 @@ void usercontrol(void) {
     //outtake press
     Controller.ButtonDown.pressed(intakeSpinAga);
     Controller.ButtonDown.released(intakeStop);
-
     //clamp code
     Controller.ButtonL2.pressed(clamped);
     Controller.ButtonL2.released(unclamped);
