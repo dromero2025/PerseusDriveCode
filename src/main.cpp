488367d19d3f17ad849113786adb1c8d499881e4
Dee <dromero@bancroftschool.org>
2024-11-21 22:41:16 -0500

    setup aivision commands for PID

--- src/main.cpp
+++ src/main.cpp
@@ -44,178 +44,91 @@ motor_group intake = motor_group(hIntMotor, lIntMotor);
 motor_group drivetake = motor_group(fLMotor, rLMotor, topLMotor, fRMotor, rRMotor, topRMotor, hIntMotor, lIntMotor);
 
 //sensor intializations
-aivision windshield = aivision(PORT6);
-  //red ring
-  aivision::colordesc RED1 = aivision::colordesc(1, 168, 9, 59, 28, 0.2);
-  aivision::colordesc RED2 = aivision::colordesc(2, 203, 32, 95, 10, 0.2);
-  aivision::colordesc RED3 = aivision::colordesc(3, 219, 46, 106, 10, 0.2);
-  aivision::colordesc RED4 = aivision::colordesc(4, 245, 121, 184, 10, 0.2);
-  color RedAvg = color((168 + 203 + 219 + 245)/4, (9 + 32 + 46 + 121)/4, (59 + 95 + 106 + 184)/4);
+aivision windshieldLeft = aivision(PORT9, aivision::ALL_AIOBJS);
+aivision winshieldRight = aivision(PORT3, aivision::ALL_AIOBJS);
 
-  aivision::codedesc redRing = aivision::codedesc(1, RED1, RED2, RED3, RED4);
-  //blue ring
+  enum VisionSignatures {
+    mobileGoal,
+    redRing,
+    blueRing
+  };
 
-  //green goal
-  aivision::colordesc GREEN0 = aivision::colordesc(5, 124, 241, 93, 10, 0.2);
-  aivision::colordesc GREEN1 = aivision::colordesc(6, 101, 199, 82, 10, 0.2);
+  // all object descriptions for game elements
+  aivision::aiobjdesc mobileGoalObj = aivision::aiobjdesc(VisionSignatures::mobileGoal);
+  aivision::aiobjdesc redRingObj = aivision::aiobjdesc(VisionSignatures::redRing);
+  aivision::aiobjdesc blueRingObj = aivision::aiobjdesc(VisionSignatures::blueRing);
 
-  aivision::codedesc goal = aivision::codedesc(3, GREEN0, GREEN1);
-distance back = distance(PORT6);
+  //aivision properties
+  float aiXFov = 74;
+  float aiYFov = 63;
 
-//intake redirect intitializations
-motor liftLeft = motor(PORT2, ratio18_1, true);
-motor liftRight = motor(PORT9, ratio18_1, false);
-motor_group lift = (liftLeft, liftRight);
+  int xRes = 320;
+  int yRes = 240; 
 
-//clamp initializations
-bool clamp = false;
-bool R1down = false;
-pneumatics mogoClamp = pneumatics(Brain.ThreeWirePort.A);
+  float xResFovRatio = aiXFov / xRes;
+  float yResFovRatio = aiYFov / yRes;
 
-//methods
+  float camDist = 10.5;
+  float camCenterDist = camDist / 2; //needs to be in units of pixel not inches
 
-void xyloPrint(){
-  Brain.Screen.drawImageFromFile("xylo1 Small.png", 0, 0);
-}
-void xylo1Print(){
-  //Brain.Screen.clearScreen();
-  Brain.Screen.drawImageFromFile("xylo2.png", 0, 0);
-}
-void kamalaPrint(){
-  //Brain.Screen.clearScreen();
-  Brain.Screen.drawImageFromFile("kamala Small.png", 0, 0);
-}
+  float absolutePos;//position from center of robot
 
-  //intake methods
-void intakeSpinFor(){
-  intake.spin(directionType::fwd, 100, velocityUnits::pct);
-  //kamalaPrint();
-}
-void intakeSpinAga(){
-  intake.spin(directionType::rev, 100, velocityUnits::pct);
-  //kamalaPrint();
-}
-void intakeStop(){
-  intake.stop();
-}
-  //clamp methods
-void clamped(){
-  //mogoClamp.set(true);
-  
-  if(R1down == false){
-    mogoClamp.set(clamp);
-    clamp = !clamp;
-  }
-  //xylo1Print();
-  R1down = true;
-}
-void unclamped(){
-  R1down = false;
-}
+  float aiDistance(aivision::aiobjdesc target){
+    float distGo;
 
-  //redirect methods
-void intakeLift(){
-  lift.spin(directionType::fwd, 100, velocityUnits::pct);
-  //xyloPrint();
-}
-void intakeLiftBack(){
-  lift.spin(directionType::rev, 100, velocityUnits::pct);
-  //xyloPrint();
-}
-void intakeDownOne(){
-  lift.setStopping(hold);
-  lift.stop();
-}
-void intakeDownTwo(){
-  lift.setStopping(coast);
-  lift.stop();
-}
+    windshieldLeft.takeSnapshot(target, 8);
+    float leftWidth = windshieldLeft.objects[0].width;
+    float leftXPos; //relative to center of camera
+    tanLeft = tan(leftWidth * xResFovRatio);  
 
-//auton methods
-void redDetect(){
-  windshield.takeSnapshot(redRing, 8);
-  if(windshield.objectCount > 0){
-    Brain.Screen.drawRectangle(windshield.objects[0].originX, windshield.objects[0].originY, windshield.objects[0].width, windshield.objects[0].height, RedAvg);
-  } else {
-    Brain.Screen.print("failed");
-  }
-}
-void approachRed(){
-  redDetect();
-  while(windshield.objects[0].originX != 0){
-    leftDrive.spin(directionType::rev, 50, velocityUnits::pct);
-    leftDrive.spin(directionType::fwd, 50, velocityUnits::pct);
-  }
+    windshieldRight.takeSnapshot(target, 8);
+    float rightWidth = windshieldRight.objects[0].width;
+    float rightXPos; //relative to center of camera
+    tanRight = tan(rightWidth * xResFovRatio);
 
-}
+    if(windshieldLeft.objects[0].centerX > xRes/2){
+      leftXPos = windshieldLeft.objects[0].centerX - xRes/2;
+    } else if(windshieldLeft.objects[0].centerX < xRes/2){
+      leftXPos = xRes/2 - windshieldLeft.objects[0].centerX;
+    } else {
+      leftXPos = 0;
+    }
 
-float avgTemp(){
-  while(true){
-    float rightDriveTemp = (fRMotor.temperature(temperatureUnits::celsius)+rRMotor.temperature(temperatureUnits::celsius)+topRMotor.temperature(temperatureUnits::celsius))/3.0;
-    float leftDriveTemp = (fLMotor.temperature(temperatureUnits::celsius)+rLMotor.temperature(temperatureUnits::celsius)+topLMotor.temperature(temperatureUnits::celsius))/3.0;
-    float driveTemp = (rightDriveTemp + leftDriveTemp)/2.0;
-    float intakeTemp = (lIntMotor.temperature(temperatureUnits::celsius)+hIntMotor.temperature(temperatureUnits::celsius))/2.0;
-    Brain.Screen.setFont(monoL);
-    Brain.Screen.print("right drive heat: ");
-    Brain.Screen.print(rightDriveTemp);
-    Brain.Screen.newLine();
-    Brain.Screen.print("left drive heat: ");
-    Brain.Screen.print(leftDriveTemp);
-    Brain.Screen.newLine();
-    Brain.Screen.print("drive heat: ");
-    Brain.Screen.print(driveTemp);
-    Brain.Screen.newLine();
-    Brain.Screen.print("intake heat: ");
-    Brain.Screen.print(intakeTemp);
-    Brain.Screen.setCursor(1,1);
-  }
-}
+    if(windshieldRight.objects[0].centerX > xRes/2){
+      rightXPos = windshieldRight.objects[0].centerX - xRes/2;
+    } else if(windshieldRight.objects[0].centerX < xRes/2){
+      rightXPos = xRes/2 - windshieldRight.objects[0].centerX;
+    } else {
+      rightXPos = 0;
+    }
+
+    absolutePos = leftXPos - camCenterDist;
+
+    distGo = (absolutePos)/(tanLeft + tanRight);
 
 
+    return distGo;
+  } //needs to return inches (units of PID)
 
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
+  float aiRotation(aivision::aiobjdesc target){
+    float theta; 
+    float centerOffset = absolutePos;
 
-void pre_auton(void) {
-  mogoClamp.set(true);
-  inert.calibrate();
-  drive.resetPosition();
-  intake.resetPosition();
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
-}
+    if((xRes/2 + camCenterDist) > leftWindshield.objects[0].centerX){
+      centerOffset *= -1;
+    } else if((camCenterDist) < rightWindshield.objects[0].centerX){
+      centerOffset *= 1;
+    } else {
+      theta = 0;
+    }
+
+    theta = xResFovRatio * centerOffset;
 
 
+    return theta;
+  } 
 
 
-//P-controller code, p only cause its not a steady state operation
-  //PD controller future? how much do you accelerate toward it. autonomous
-  //D for driver makes controller not linear, harder to drive
-
-/*
-  currentRotZ = current inertial heading
-  fwdTarget = joystick value mapped from -1.0 to 1.0
-  fwdConstant = experimentally determined "how fast can you increment per loop time"
-  rotZIncrement = experimentally determined "what is multiplied by the joystick value of turning and added to your rotz target"
-  kp * 90 = normal PID constant
-  
-
-  citation: Mr. Harrington, based off code he wrote for an Arduino-based puppy bot
-  "https://github.com/BancroftSchoolOpenSource/ArduinoClassRobot/blob/main/ArduinoClassRobot.ino"
-
-  citation: Adam Vining-Recklitis, based off code he wrote for a robot last year
-  "https://github.com/BancroftRoboDogs/RobodogsCode_2023_03/blob/main/test/src/main.cpp"
-  thank you especially to Adam, for allowing me to work off his stuff, love ya buddy
-*/
-
-//PID
 class PID{
  public:
    double inertVal = 0.0;
@@ -288,8 +201,8 @@ class PID{
        derivative = error - prevError;
        prevError = error;
        output = error * kP + integral * kI + derivative * kD;
-       leftDrive.spin(directionType::fwd, output, voltageUnits::volt);
        rightDrive.spin(directionType::fwd, output, voltageUnits::volt);
+       leftDrive.spin(directionType::fwd, output, voltageUnits::volt);
        printf("moveto: %f \n",error);
        if(error < 5 && error > (-5)){
          settle++;
@@ -307,8 +220,125 @@ class PID{
      rightDrive.resetPosition();
    }
 };
+//intake redirect intitializations
+motor liftLeft = motor(PORT2, ratio18_1, true);
+motor liftRight = motor(PORT9, ratio18_1, false);
+motor_group lift = (liftLeft, liftRight);
+
+//clamp initializations
+bool clamp = false;
+bool doinker = false;
+bool Adown = false;
+bool R1down = false;
+pneumatics mogoClamp = pneumatics(Brain.ThreeWirePort.A);
+pneumatics doink = pneumatics(Brain.ThreeWirePort.B);
+
+//methods
+  //intake methods
+void intakeSpinFor(){
+  intake.spin(directionType::fwd, 100, velocityUnits::pct);
+  //kamalaPrint();
+}
+void intakeSpinAga(){
+  intake.spin(directionType::rev, 100, velocityUnits::pct);
+  //kamalaPrint();
+}
+void intakeStop(){
+  intake.stop();
+}
+  //clamp methods
+void clamped(){
+  //mogoClamp.set(true);
+  
+  if(R1down == false){
+    mogoClamp.set(clamp);
+    clamp = !clamp;
+  }
+  //xylo1Print();
+  R1down = true;
+}
+void unclamped(){
+  R1down = false;
+}
+  //doink methods
+void doinked(){
+  //mogoClamp.set(true);
+  
+  if(Adown == false){
+    doink.set(doinker);
+    doinker = !doinker;
+  }
+  //xylo1Print();
+  Adown = true;
+}
+void undoink(){
+  Adown = false;
+}
+
+  //redirect methods
+void intakeLift(){
+  lift.spin(directionType::fwd, 100, velocityUnits::pct);
+  //xyloPrint();
+}
+void intakeLiftBack(){
+  lift.spin(directionType::rev, 100, velocityUnits::pct);
+  //xyloPrint();
+}
+void intakeDownOne(){
+  lift.setStopping(hold);
+  lift.stop();
+}
+void intakeDownTwo(){
+  lift.setStopping(coast);
+  lift.stop();
+}
+
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton(void) {
+  mogoClamp.set(true);
+  inert.calibrate();
+  drive.resetPosition();
+  intake.resetPosition();
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+
+
+
+//P-controller code, p only cause its not a steady state operation
+  //PD controller future? how much do you accelerate toward it. autonomous
+  //D for driver makes controller not linear, harder to drive
+
+/*
+  currentRotZ = current inertial heading
+  fwdTarget = joystick value mapped from -1.0 to 1.0
+  fwdConstant = experimentally determined "how fast can you increment per loop time"
+  rotZIncrement = experimentally determined "what is multiplied by the joystick value of turning and added to your rotz target"
+  kp * 90 = normal PID constant
+  
+
+  citation: Mr. Harrington, based off code he wrote for an Arduino-based puppy bot
+  "https://github.com/BancroftSchoolOpenSource/ArduinoClassRobot/blob/main/ArduinoClassRobot.ino"
+
+  citation: Adam Vining-Recklitis, based off code he wrote for a robot last year
+  "https://github.com/BancroftRoboDogs/RobodogsCode_2023_03/blob/main/test/src/main.cpp"
+  thank you especially to Adam, for allowing me to work off his stuff, love ya buddy
+*/
+
+//PID
 PID turning = PID(0.3, 0.0, 0.0);
-PID moving = PID(0.4, 0.0, 0.0);
+PID moving = PID(0.5, 0.0, 0.0);
 
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
@@ -322,82 +352,91 @@ PID moving = PID(0.4, 0.0, 0.0);
 
 void autonomous(void){ 
   intake.setVelocity(100,velocityUnits::pct);
+  drive.resetPosition();
+  int slotter = 1;
 
 
-  //RIGHT SIDE
-  //drive towards mobile goal
-  drive.spinFor(directionType::rev, 900, rotationUnits::deg, 50, velocityUnits::pct);
-  drive.stop();
-  //clamp goal
-  mogoClamp.set(false);
-  //load preload onto goal
-  intake.spinFor(directionType::fwd, 1000, rotationUnits::deg, 100, velocityUnits::pct);
-  drive.resetPosition();
-  //turn right
-  leftDrive.spinFor(directionType::rev, 225, rotationUnits::deg, 25, velocityUnits::pct);
-  rightDrive.spinFor(directionType::rev, -400, rotationUnits::deg, 25, velocityUnits::pct);
-  drive.resetPosition();
-  //intake ring
-  intake.spin(directionType::fwd);
-  drive.spinFor(directionType::fwd, 1500, rotationUnits::deg, 100, velocityUnits::pct);
-  wait(375, timeUnits::msec);
-  //drive back towards tower
-  intake.stop();
-  drive.spinFor(directionType::rev, 1650, rotationUnits::deg, 100, velocityUnits::pct);
-  drive.resetPosition();
-  wait(1000, timeUnits::msec);
-  intake.stop();
+  // //RIGHT SIDE
+  // //drive towards mobile goal
+  // drive.spinFor(directionType::rev, 900, rotationUnits::deg, 50, velocityUnits::pct);
+  // drive.stop();
+  // //clamp goal
+  // mogoClamp.set(false);
+  // //load preload onto goal
+  // intake.spinFor(directionType::fwd, 1000, rotationUnits::deg, 100, velocityUnits::pct);
+  // drive.resetPosition();
+  // //turn right
+  // leftDrive.spinFor(directionType::rev, 225, rotationUnits::deg, 25, velocityUnits::pct);
+  // rightDrive.spinFor(directionType::rev, -400, rotationUnits::deg, 25, velocityUnits::pct);
+  // drive.resetPosition();
+  // //intake ring
+  // intake.spin(directionType::fwd);
+  // drive.spinFor(directionType::fwd, 1500, rotationUnits::deg, 100, velocityUnits::pct);
+  // wait(375, timeUnits::msec);
+  // //drive back towards tower
+  // intake.stop();
+  // drive.spinFor(directionType::rev, 1650, rotationUnits::deg, 100, velocityUnits::pct);
+  // drive.resetPosition();
+  // wait(1000, timeUnits::msec);
+  // intake.stop();
 
   //RIGHT SIDE W/Intertial
-  //move towards goal
-  moving.moveTo(24.0);
-  //clamp
-  mogoClamp.set(false);
-  //intake preload onto goal, keep intake running
-  intake.spin(directionType::fwd);
-  //turn towards ring stack
-  turning.turnTo(90.0);
-  //move into rings
-  moving.moveTo(24);
+  if (slotter == 1){
+    //move towards goal
+    moving.moveTo(24);
+    //clamp
+    mogoClamp.set(false);
+    //intake preload onto goal, keep intake running
+    intake.spin(directionType::fwd);
+    //turn towards ring stack
+    turning.turnTo(90);
+    //move into rings
+    moving.moveTo(24);
+  }
 
-  //LEFT SIDE
-  //drive towards mobile goal
-  drive.spinFor(directionType::rev, 900, rotationUnits::deg, 50, velocityUnits::pct);
-  drive.stop();
-  //clamp goal
-  mogoClamp.set(false);
-  //load preload onto goal
-  intake.spinFor(directionType::fwd, 1000, rotationUnits::deg, 100, velocityUnits::pct);
-  drive.resetPosition();
-  //turn left
-  rightDrive.spinFor(directionType::rev, 225, rotationUnits::deg, 25, velocityUnits::pct);
-  leftDrive.spinFor(directionType::rev, -400, rotationUnits::deg, 25, velocityUnits::pct);
-  drive.resetPosition();
-  //intake ring
-  intake.spin(directionType::fwd);
-  drive.spinFor(directionType::fwd, 1500, rotationUnits::deg, 100, velocityUnits::pct);
-  wait(375, timeUnits::msec);
-  //drive back towards tower
-  intake.stop();
-  drive.spinFor(directionType::rev, 1650, rotationUnits::deg, 100, velocityUnits::pct);
-  drive.resetPosition();
-  wait(1000, timeUnits::msec);
-  intake.stop();
-  
+  // //LEFT SIDE
+  // //drive towards mobile goal
+  // drive.spinFor(directionType::rev, 900, rotationUnits::deg, 50, velocityUnits::pct);
+  // drive.stop();
+  // //clamp goal
+  // mogoClamp.set(false);
+  // //load preload onto goal
+  // intake.spinFor(directionType::fwd, 1000, rotationUnits::deg, 100, velocityUnits::pct);
+  // drive.resetPosition();
+  // //turn left
+  // rightDrive.spinFor(directionType::rev, 225, rotationUnits::deg, 25, velocityUnits::pct);
+  // leftDrive.spinFor(directionType::rev, -400, rotationUnits::deg, 25, velocityUnits::pct);
+  // drive.resetPosition();
+  // //intake ring
+  // intake.spin(directionType::fwd);
+  // drive.spinFor(directionType::fwd, 1500, rotationUnits::deg, 100, velocityUnits::pct);
+  // wait(375, timeUnits::msec);
+  // //drive back towards tower
+  // intake.stop();
+  // drive.spinFor(directionType::rev, 1650, rotationUnits::deg, 100, velocityUnits::pct);
+  // drive.resetPosition();
+  // wait(1000, timeUnits::msec);
+  // intake.stop();
+
   //LEFT SIDE W/Intertial
+  if (slotter == 2){
+    drive.resetPosition();
   //move towards goal
-  moving.moveTo(24.0);
+  moving.moveTo(-42);
   //clamp
   mogoClamp.set(false);
+  //turn towards ring stack
+  turning.turnTo(-130);
   //intake preload onto goal, keep intake running
   intake.spin(directionType::fwd);
-  //turn towards ring stack
-  turning.turnTo(-90.0);
   //move into rings
+  drive.resetPosition();
+  moving.moveTo(24);
+  turning.turnTo(-90);
   moving.moveTo(24);
 
+  }
 
-  //Theoretical Left Side With PID
 
 }
 
@@ -418,6 +457,7 @@ void usercontrol(void) {
   // User control code here, inside the loop
   while (1) {
   
+
   //drivetrain code
     leftDrive.spin(directionType::fwd, (Controller.Axis3.value() + Controller.Axis1.value()) * driveChanger, velocityUnits::pct);
     rightDrive.spin(directionType::fwd, (Controller.Axis3.value() - Controller.Axis1.value()) * driveChanger, velocityUnits::pct);
@@ -432,6 +472,9 @@ void usercontrol(void) {
     //clamp code
     Controller.ButtonL2.pressed(clamped);
     Controller.ButtonL2.released(unclamped);
+    //doink code
+    Controller.ButtonA.pressed(doinked);
+    Controller.ButtonA.released(undoink);
     //redirect code
     Controller.ButtonR1.pressed(intakeLift);
     Controller.ButtonR2.pressed(intakeLiftBack);
